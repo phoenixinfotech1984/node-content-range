bb93331fcf5dfe462d1267349e2ef3069262bbdc
Vishal Patel <phenix.infotech1984@gmail.com>
2020-01-12 02:52:03 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const { stat, createReadStream } = require("fs");
+const { promisify } = require("util");
+const sampleVideo = "./main.mp4";
+const fileInfo = promisify(stat);
+
+http
+  .createServer(async (req, res) => {
+    /** Calculate Size of file */
+    const { size } = await fileInfo(sampleVideo);
+
+    const range = req.headers.range;
+
+    /** Check for Range header */
+    if (range) {
+      /** Extracting Start and End value from Range Header */
+      let [start, end] = range.replace(/bytes=/, "").split("-");
+      start = parseInt(start, 10);
+      end = end ? parseInt(end, 10) : size - 1;
+
+      if (!isNaN(start) && isNaN(end)) {
+        start = start;
+        end = size - 1;
+      }
+
+      if (isNaN(start) && !isNaN(end)) {
+        start = size - end;
+        end = size - 1;
+      }
+
+      /** Sending Partial Content With HTTP Code 206 */
+      res.writeHead(206, {
+        "Content-Range": `bytes ${start}-${end}/${size}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": end - start + 1,
+        "Content-Type": "video/mp4"
+      });
+
+      createReadStream(fileName, { start, end }).pipe(res);
+    } else {
+      res.writeHead(200, {
+        "Content-Length": size,
+        "Content-Type": "video/mp4"
+      });
+      createReadStream(sampleVideo).pipe(res);
+    }
+  })
+  .listen(port, () => console.log("Running on 3000 port"));
